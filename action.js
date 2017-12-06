5377fd2c1984450571fa1200079e17aba30cc73d
stevensadh95 <stevensadh95@gmail.com>
2017-12-05 19:00:38 -0500

    changes

--- action.js
+++ action.js
@@ -1,16 +1,54 @@
+var data_array;
+
+$('#formId').submit(function() {
+   let inputval = $("input:first").val()
+   
+   // example request
 
 
-$.ajax({
-  url: "localhost",  // whatever the url to ruby web server is
-  type: "get", //send data through get method
-  data: { 
-    Name: 4, // populate name with the one given
-  },
-  success: function(response) {
-    //Do Something
-  },
-  error: function(xhr) {
-    //Do Something to handle error
-  }
+    $.get("http://127.0.0.1:4567/name", {name:inputval}).done((data)=> {   alert("success") })
+                    
+        .fail((xhr) => {
+                        alert('Problem contacting server');
+                        console.log(xhr);
+                    });
 });
 
+var ctx = document.getElementById("myChart");
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["1900", "1910", "1920", "1930", "1940", "1950","1960", "1970", "1980", "1990", "2000"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3, 6, 8, 9, 0, 12],  // replace this with returned ruby stuff
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
--- final.rb
+++ final.rb
@@ -9,6 +9,11 @@ end
 get '/name' do
 
   name = params["name"]
-
+  
+  if name == "steve"
+  list = [1,2,3,4,5,6]
+  
+  else
+  list = [10,9,8,7,6,5]
   #TODO: query database with name and return result (May have to be a JSON file or other format)
 end
